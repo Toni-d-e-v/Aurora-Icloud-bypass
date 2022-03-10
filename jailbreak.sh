9b2fa228c3d4e6a1931f02d3f0a945d985536b70
toni-dev <62844491+Toni-d-e-v@users.noreply.github.com>
2022-03-10 13:22:48 +0100

    Create jailbreak.sh

---
+++ jailbreak.sh
@@ -0,0 +1,10 @@
+echo "Getting ready to jailbreak..."
+sudo ./source/exe/jk
+echo "If jailbreak was successfull you should continue"
+read -p "Press button R to restart jailbreak or press enter to continue"
+if [ "$REPLY" == "r" ]; then
+  echo "Restarting jailbreak..."
+  sudo ./source/exe/jk
+else 
+  echo "You selected to continue"
+fi
