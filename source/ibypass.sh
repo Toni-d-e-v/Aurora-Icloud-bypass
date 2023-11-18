0f20b21c2e71840284faa2773e773e05a2bcc25e
toni-dev <62844491+Toni-d-e-v@users.noreply.github.com>
2023-11-18 09:59:56 +0100

    Update ibypass.sh

--- source/ibypass.sh
+++ source/ibypass.sh
@@ -1,58 +1,72 @@
-# icloud unlock Aurora: 0.1
-echo "Welcome to icloud unlock Aurora: 0.1, github.com/Toni-d-e-v/Icloud-Unlocker/"
-echo "To make this exploit work we will launch checkrain jailbreak please follow the instructions and do not close the terminal"
-echo "If you want to exit the script press ctrl + c"
+#!/bin/bash
+
+# iCloud Unlock Aurora 0.1
+echo "Welcome to iCloud Unlock Aurora 0.1, github.com/Toni-d-e-v/Icloud-Unlocker/"
+echo "To make this exploit work, we will launch checkra1n jailbreak. Please follow the instructions and do not close the terminal."
+echo "If you want to exit the script, press Ctrl+C."
 read -p "Press enter to continue"
+
+# Checking for python3
 echo "Checking for python3..."
-if ! [ -x "$(command -v python3)" ]; then
+if ! command -v python3 &> /dev/null; then
   echo 'Error: python3 is not installed.' >&2
   exit 1
 else
   echo "python3 is installed"
 fi
+
+# Checking for pip3
 echo "Checking for pip3..."
-if ! [ -x "$(command -v pip3)" ]; then
+if ! command -v pip3 &> /dev/null; then
   echo 'Error: pip3 is not installed.' >&2
   exit 1
 else
   echo "pip3 is installed"
 fi
-echo "Getting ready to jailbreak..."pip paramiko
+
+# Getting ready to jailbreak
 sudo ./source/exe/jk
-echo "If jailbreak was successfull you should continue"
-read -p "Press button R to restart jailbreak or press enter to continue"
-if [ "$REPLY" == "r" ]; then
+
+# Restarting jailbreak if requested
+read -p "Press 'R' to restart jailbreak or press enter to continue"
+if [ "${REPLY,,}" == "r" ]; then
   echo "Restarting jailbreak..."
   sudo ./source/exe/jk
-else 
+else
   echo "You selected to continue"
 fi
-echo "Checking for python scripts..."
-if [ -f "./source/scripts/bypass.py" ]; then
-  echo "bypass.py found"
-else
-  echo "bypass.py not found"
-  exit 1
-fi
-if [ -f "./source/scripts/usbmux.py" ]; then
-  echo "usbmux.py found"
-else
-  echo "usbmux.py not found"
-  exit 1
-fi
+
+# Checking for python scripts
+for script in "./source/scripts/bypass.py" "./source/scripts/usbmux.py"; do
+  if [ -f "$script" ]; then
+    echo "$script found"
+  else
+    echo "$script not found"
+    exit 1
+  fi
+done
+
+# Installing required libs
 echo "Installing required libs..."
 sudo pip3 install paramiko
+
+# All files are found
 echo "All files are found"
 echo "Good luck"
+
+# Starting python scripts
 echo "Starting python scripts..."
-echo "Let the script finish wait 2-5 minutes and your device should be unlocked"
-echo "ON COMPLEATE REBOOT DEVICE MAY BE LOCKED, AND SIM CARD WONT WORK"
-echo "ON SOME IOS VERSIONS THIS WONT WORK AND PLEASE DO NOT UPDATE, IF IT DOES NOT WORK DOWNGRADE IOS"
-# coutdown
-for i in {1..5}
-do
-  echo "Starting at 5 $i"
+echo "Let the script finish. Wait 2-5 minutes, and your device should be unlocked."
+echo "ON COMPLETE REBOOT DEVICE MAY BE LOCKED, AND SIM CARD WON'T WORK"
+echo "ON SOME IOS VERSIONS THIS WON'T WORK. PLEASE DO NOT UPDATE; IF IT DOES NOT WORK, DOWNGRADE IOS"
+
+# Countdown
+for i in {1..5}; do
+  echo "Starting at $i"
   sleep 1
 done
+
+# Running bypass.py
 python3 ./source/scripts/bypass.py
+
 exit 0
